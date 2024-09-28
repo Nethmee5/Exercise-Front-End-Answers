5132255170fde5b149490c1cd2581cb62e5aca24
Nethmee5 <kumararatnenethmee@gmail.com>
2024-09-29 05:51:53 +0800

    updated

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,28 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route, useNavigate } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  useNavigate,
+} from "react-router-dom";
 import Calculator from "./pages/answers/Calculator";
 import Navbar from "./pages/answers/Navbar";
 import Question3 from "./pages/answers/Question3";
 
-const NavigationButtons: React .FC = () => {
+const NavigationButtons: React.FC = () => {
   const Navigate = useNavigate();
-  return(
+  return (
     <div>
-      <button onClick={()=>Navigate('/')}>Go to Calculator</button>
-      <button onClick={()=>Navigate('/Navbar')}>Go to Navbar</button>
-      <button onClick={()=>Navigate('/Question3')}>Go to Question3</button>
+      <button onClick={() => Navigate("/")}>Go to Calculator</button>
+      <button onClick={() => Navigate("/Navbar")}>Go to Navbar</button>
+      <button onClick={() => Navigate("/Question3")}>Go to Question3</button>
     </div>
   );
 };
 const App: React.FC = () => {
   return (
     <Router>
-      <NavigationButtons/>
+      <NavigationButtons />
       <Routes>
         <Route path="/" element={<Calculator />} />
         <Route path="Calculator" element={<Calculator />} />
